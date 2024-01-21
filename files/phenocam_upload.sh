8de34e9149017aa7f5796d6a6bb9cc8cc68de246
Koen Hufkens <koen.hufkens@gmail.com>
2024-01-21 18:43:57 +0100

    new upload

--- files/phenocam_upload.sh
+++ files/phenocam_upload.sh
@@ -1,105 +1,165 @@
 #!/bin/sh
 
-echo "Trying to capture a raw image" >> /var/tmp/log.txt
+#--------------------------------------------------------------------
+# This script is cued up in the crontab file and called every
+# x min to upload two images, a standard RGB image and an infra-
+# red (IR) image (if available) to the PhenoCam server.
+#
+# (c) Koen Hufkens for BlueGreen Labs
+#--------------------------------------------------------------------
 
-debugmode="yes"
-loopaddr="admin:phen0cam@127.0.0.1"
+# -------------- SETTINGS -------------------------------------------
 
-tempdir=/var/tmp  #our temporary working folder.
-mkdir -p $tempdir #This folder likely exists, but make sure.
-cd $tempdir       #use the dram, don't wear out flash. /var/tmp most space
+# read in configuration settings
+# grab sitename
+SITENAME=`awk 'NR==1' /mnt/cfg1/settings.txt`
 
-getEXIF()
-{
- i2cset -yf 0 0x21 0x20 1 #buffer sequential bytes of EXIF info. 
- swl=`i2cget -yf 0 0x21 0x20 | cut -b3-4 2>> /var/tmp/log.txt` #low exposure (shut width)
- swh=`i2cget -yf 0 0x21 0x20 2>> /var/tmp/log.txt` #high exposure number of rows
- shutwidth=$swh$swl #Full exposure in ascii hex (e.g. 0x0464)
- rpsl=`i2cget -yf 0 0x21 0x20 | cut -b3-4 2>> /var/tmp/log.txt` #low
- rpsh=`i2cget -yf 0 0x21 0x20 2>> /var/tmp/log.txt` #high 
- rowspersec=$rpsh$rpsl #rows per second in ascii hex (e.g. 0x83D6)
- bll=`i2cget -yf 0 0x21 0x20 | cut -b3-4 2>> /var/tmp/log.txt` #low black level
- blh=`i2cget -yf 0 0x21 0x20 2>> /var/tmp/log.txt` #high 
- blacklev=$blh$bll #blacklevel (pedestal e.g. 0x0048)
-}
+# how many servers do we upload to
+nrservers=`awk 'END {print NR}' /mnt/cfg1/server.txt`
 
-#Shows EXIF info, after it's gathered. Only in debugmode=yes.
-showEXIF()
-{
-if [ $debugmode = "yes" ]; then
- echo Shutter width ${shutwidth}
- echo Rows per second ${rowspersec}
- echo Black level ${blacklev}
-fi
-}
+# Move into temporary directory
+# which resides in RAM, not to
+# wear out other permanent memory
 
-#Capture a raw dump (It's not a DNG yet). 
-# Returns $rawname= file made, or "none"
+cd /var/tmp
 
-getDUMP()
-{ 
- echo vcap0_0_0_2 o $tempdir > /proc/videograph/gs/dump #make raw pic
- datetimestring=`date +"%Y_%m_%d_%H%M%S"`
- for i in 1 2 3 #if it goes over 3 secs, abort!
- do
-  sleep 1s  #1s should do it for dump, most of the time.
-  dumpfile=`ls vcap0_0_0_2* 2>> /var/tmp/log.txt`  
-  if [ -z "$dumpfile" ]
-  then
-   echo Extra sleep needed for vcap.
-   rawname="none"
-  else
-   rawname=mycamera_${datetimestring}.dng
-   mv $dumpfile $rawname  
-   break
-  fi
- done
-}
+# export to current clock settings
+export TZ=`cat /etc/config/TZ`
 
-#Start of script to upload 2 images, one with IR closed, one open.
-#First clean oldd files, save IR state, make open and closed values
-find . -name "vcap0_0_0_2*" -delete #delete leftovers
-oldreg7=`i2cget -yf 0 0x21 7 2>> /var/tmp/log.txt` #save IR reg contents
-irclose=`echo $oldreg7 | sed s/./0/3` #Replace top nibble with IR closed 
-iropen=`echo $oldreg7 | sed s/./1/3` #Replace top nibble with IR open bits
-
-echo $oldreg7 >> /var/tmp/log.txt
-echo $irclose >> /var/tmp/log.txt
-echo $iropen >> /var/tmp/log.txt
-
-#Close IR so it can settle before we lock exposure with raw mode.
-i2cset -yf 0 0x21 7 $irclose 2>> /var/tmp/log.txt #IR closed
-sleep 1s #needs time to close and adjust exposure. 
-#raw mode increases H.264 size and we need time to expand buffers
-#if it's the first time this ran. 
-
-echo Entering raw mode...
-i2cset -y -f 0 0x21 0x1F 1 2>> /var/tmp/log.txt #trigger raw mode, lock exposure
-find . -name vb.htm* -delete #clean up possible leftovers: wget won't overwrite.
-wget http://${loopaddr}/vb.htm?EnableTextForRaw=0 #Overlay off. Note: causes output on concole of 0, 0 and 0 on console.
-find . -name "vb.*" -delete
-sleep 4 #overlay goes off in < 2 sec, but raw mode causes need for buffer increases, so wait.
-
-#grab EXIF info for these pics.  shutwdth,exprows wont change. blklv can. 
-getEXIF
-showEXIF >> /var/tmp/log.txt
-getDUMP
-
-if [ $rawname = "none" ]; then
- echo FAILED to make first raw picture! >> /var/tmp/log.txt
- exit 1 #oops. didnt make first pic.
+# switch time zone sign
+if [ -n `echo $TZ | grep -` ]; then
+	TZONE=`echo "$TZ" | sed 's/+/-/g'`
+else
+	TZONE=`echo "$TZ" | sed 's/-/+/g'`
 fi
 
-echo Exiting raw mode...
-i2cset -y -f 0 0x21 0x1F 2 2>> /var/tmp/log.txt #exit raw mode, free up visual images mode and exposure.
-wget http://${loopaddr}/vb.htm?textenable1
-overlaystate=`cat vb.htm?textenable1 | grep -o '.\{1\}$'` #Get config's overlay state
-find . -name vb.htm* -delete  
-wget http://$loopaddr/vb.htm?EnableTextForRaw=$overlaystate #Set it, without flash write.
-find . -name "vb.*" -delete
+# sets the delay between the
+# RGB and IR image acquisitions
+DELAY=30
 
+# sets debug state, for script development only
+# operational value is 0
+DEBUG=1
+if [ "$DEBUG" = "1" ] ; then
+LOG="/var/tmp/IR_upload.log"
+rm -f $LOG > /dev/null
+else
+LOG="/dev/null"
+fi
 
-echo First raw picture! >> /var/tmp/log.txt
-#ftpput $vb -u anonymous -p "anonymous"  ${uploadfolder}/${rawname} ${rawname}
-rm ${rawname}
+# how many servers do we upload to
+nrservers=`awk 'END {print NR}' server.txt`
+
+# -------------- UPLOAD IMAGES --------------------------------------
+
+# grab date - keep fixed for RGB and IR uploads
+DATE=`date +"%a %b %d %Y %H:%M:%S"`
+
+# grap date and time string to be inserted into the
+# ftp scripts - this coordinates the time stamps
+# between the RGB and IR images (otherwise there is a
+# slight offset due to the time needed to adjust exposure
+DATETIMESTRING=`date +"%Y_%m_%d_%H%M%S"`
+
+# grab date and time for `.meta` files
+METADATETIME=`date -Iseconds`
+
+# grab metadata using the metadata function
+# grab the MAC address
+mac_addr=`ifconfig eth0 | grep HWaddr | awk '{print $5}' | sed 's/://g'`
+
+# grab internal ip address
+ip_addr=`ifconfig eth0 | awk '/inet addr/{print substr($2,6)}'`
+
+# grab external ip address if there is an external connection
+# first test the connection to the google name server
+connection=`ping -q -c 1 8.8.8.8 > /dev/null && echo ok || echo error`
+
+# -------------- UPLOAD VIS -----------------------------------------
+
+# create filenames
+metafile=`echo ${SITENAME}_${DATETIMESTRING}.meta`
+image=`echo ${SITENAME}_${DATETIMESTRING}.jpg`
+
+# create base meta-data file from configuration settings
+cat /mnt/cfg1/settings.txt > /var/tmp/${metafile}
+
+# append meta-data
+echo "ip_addr=$ip_addr" >> /var/tmp/${metafile}
+echo "mac_addr=$mac_addr" >> /var/tmp/${metafile}
+echo "datetime_original=\"$METADATETIME\"" >> /var/tmp/${metafile}
+
+# Set the image to non IR i.e. VIS
+/usr/sbin/set_ir.sh 0
+
+# adjust exposure
+sleep $DELAY
+
+# grab the image from the
+wget http://127.0.0.1/image.jpg -O ${image}
+
+# grab the exposure time and append to meta-data
+exposure=`/usr/sbin/get_exp`
+echo $exposure >> /var/tmp/${metafile}
+
+# run the upload script for the ip data
+# and for all servers
+for i in `seq 1 $nrservers` ;
+do
+	SERVER=`awk -v p=$i 'NR==p' /mnt/cfg1/server.txt`
+	
+	# upload image
+	ftpput ${SERVER} -u anonymous -p "anonymous"  data/${SITENAME}/${image} ${image}
+	
+	# upload meta-file
+	ftpput ${SERVER} -u anonymous -p "anonymous"  data/${SITENAME}/${metafile} ${metafile}
+done
+
+# clean up files
+rm *.jpg
+rm *.meta
+
+# -------------- UPLOAD NIR -----------------------------------------
+
+# create filenames
+metafile=`echo ${SITENAME}_IR_${DATETIMESTRING}.meta`
+image=`echo ${SITENAME}_IR_${DATETIMESTRING}.jpg`
+
+# create base meta-data file from configuration settings
+cat /mnt/cfg1/settings.txt > /var/tmp/${metafile}
+
+# append meta-data
+echo "ip_addr=$ip_addr" >> /var/tmp/${metafile}
+echo "mac_addr=$mac_addr" >> /var/tmp/${metafile}
+echo "datetime_original=\"$METADATETIME\"" >> /var/tmp/${metafile}
+
+# Set the image to NIR
+/usr/sbin/set_ir.sh 1
+
+# adjust exposure 
+sleep $DELAY
+
+# grab the image from the
+wget http://127.0.0.1/image.jpg -O ${image}
+
+# grab the exposure time and append to meta-data
+exposure=`/usr/sbin/get_exp`
+echo $exposure >> /var/tmp/${metafile}
+
+# run the upload script for the ip data
+# and for all servers
+for i in `seq 1 $nrservers` ;
+do
+	SERVER=`awk -v p=$i 'NR==p' /mnt/cfg1/server.txt` 
+
+	# upload image
+	ftpput ${SERVER} -u anonymous -p "anonymous"  data/${SITENAME}/${image} ${image}
+	
+	# upload meta-file
+	ftpput ${SERVER} -u anonymous -p "anonymous"  data/${SITENAME}/${metafile} ${metafile}
+done
+
+# clean up files
+rm *.jpg
+rm *.meta
 
