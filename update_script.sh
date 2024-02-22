0f60c03ead8606a6f1eb7144ccc93a235da1d50b
Koen Hufkens <koen.hufkens@gmail.com>
2024-02-22 20:06:11 +0000

    Update update_script.sh

--- update_script.sh
+++ update_script.sh
@@ -3,7 +3,7 @@
 tar -cf install files/*
 
 cat PITe.sh > PIT.sh
-cat install >> PIT.sh
+base64 install >> PIT.sh
 
 rm install*
 
@@ -12,5 +12,3 @@ chmod +x PIT.sh
 git add PIT.sh
 git commit -am "update PIT"
 git push
-
-
